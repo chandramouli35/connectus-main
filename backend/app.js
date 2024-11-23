8f59ea09f14464b75604f2759c0692649ff76109
chandramouli35 <tarigopulachandramouli1818@gmail.com>
2024-11-23 07:44:40 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,33 @@
+// import express from "express";
+// import cookieParser from "cookie-parser";
+// import userRoutes from "./routes/user.route.js";
+// import tweetRoutes from "./routes/tweet.route.js";
+// import { jwtTokenAuthentication } from "./config/jwtAuthController.js";
+// import cors from "cors";
+
+// const app = express();
+
+// // middlewares
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(cookieParser());
+// var corsOptions = {
+//   origin: "http://localhost:5173",
+//   // origin: 'https://connectus-frontend-tqv5.onrender.com',
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+
+// // APIs
+// app.use("/api/v1/user", userRoutes);
+// app.use("/api/v1/tweet", jwtTokenAuthentication, tweetRoutes);
+
+// export { app };
+
 import express from "express";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
@@ -7,23 +37,29 @@ import cors from "cors";
 
 const app = express();
 
-// middlewares
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-var corsOptions = {
-  origin: "http://localhost:5173",
-  // origin: 'https://connectus-frontend-tqv5.onrender.com',
+
+// CORS options
+const corsOptions = {
+  origin: [
+    "http://localhost:5173", // Local frontend
+    "https://your-frontend-domain.vercel.app", // Deployed frontend
+  ],
   credentials: true,
 };
 app.use(cors(corsOptions));
+
+// Default route for root
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
 
 // APIs
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/tweet", jwtTokenAuthentication, tweetRoutes);
 
-export { app };
+// Export app for serverless functions
+export default app;
